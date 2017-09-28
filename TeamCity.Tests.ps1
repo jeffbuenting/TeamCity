5f2c30c91a5c4b420526edb3ce14e1d6b4f95139
jeffbuenting <jeffbuenting@gmail.com>
2017-09-28 13:26:32 -0400

    Initial Commit

---
+++ TeamCity.Tests.ps1
@@ -0,0 +1,54 @@
+﻿$ModulePath = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+$ModuleName = $ModulePath | Split-Path -Leaf
+
+# ----- Remove and then import the module.  This is so any new changes are imported.
+Get-Module -Name $ModuleName -All | Remove-Module -Force -Verbose
+
+Import-Module "$ModulePath\$ModuleName.PSD1" -Force -ErrorAction Stop -Scope Global -Verbose
+
+
+#-------------------------------------------------------------------------------------
+
+Write-Output "`n`n"
+
+Describe "ControlPanel : Get-CPFolderOption" {
+    # ----- Get Function Help
+    # ----- Pester to test Comment based help
+    # ----- http://www.lazywinadmin.com/2016/05/using-pester-to-test-your-comment-based.html
+    Context "Help" {
+
+        $H = Help Get-CPFolderOption -Full
+
+        # ----- Help Tests
+        It "has Synopsis Help Section" {
+            $H.Synopsis | Should Not BeNullorEmpty
+        }
+
+        It "has Description Help Section" {
+            $H.Description | Should Not BeNullorEmpty
+        }
+
+        It "has Parameters Help Section" {
+            $H.Parameters | Should Not BeNullorEmpty
+        }
+
+        # Examples
+        it "Example - Count should be greater than 0"{
+            $H.examples.example.code.count | Should BeGreaterthan 0
+        }
+            
+        # Examples - Remarks (small description that comes with the example)
+        foreach ($Example in $H.examples.example)
+        {
+            it "Example - Remarks on $($Example.Title)"{
+                $Example.remarks | Should not BeNullOrEmpty
+            }
+        }
+
+        It "has Notes Help Section" {
+            $H.alertSet | Should Not BeNullorEmpty
+        }
+    } 
+
+}
